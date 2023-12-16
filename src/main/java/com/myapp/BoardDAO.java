c9f2d2ce78e14f818eb315fb1003deacd1b5a137
dirhq88 <126394721+dirhq88@users.noreply.github.com>
2023-12-16 17:23:19 +0900

    Update BoardDAO.java

--- src/main/java/com/myapp/BoardDAO.java
+++ src/main/java/com/myapp/BoardDAO.java
@@ -1,7 +1,7 @@
-package com.myapp;
+package com.myapp.board;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,42 +9,30 @@ import java.util.List;
 @Repository
 public class BoardDAO {
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    SqlSession sqlSession;
 
     public int insertBoard(BoardVO vo) {
-        String sql = "insert into SchoolBoard (student, studentNumber, school, schoolNumber, location, subsidy) values ("
-                + "'" + vo.getStudent() + "',"
-                + "'" + vo.getStudentNumber() + "',"
-                + "'" + vo.getSchool() + "',"
-                + "'" + vo.getSchoolNumber() + "',"
-                + "'" + vo.getLocation() + "',"
-                + "'" + vo.getSubsidy()+ "')";
-        return jdbcTemplate.update(sql);
+        int result = sqlSession.insert("Board.insertBoard", vo);
+        return result;
     }
 
     public int deleteBoard(int id) {
-        String sql = "delete from SchoolBoard  where id = " + id;
-        return jdbcTemplate.update(sql);
+        int result = sqlSession.delete("Board.deleteBoard", id);
+        return result;
     }
 
     public int updateBoard(BoardVO vo) {
-        String sql = "update SchoolBoard set student='"
-                + vo.getStudent() + "',"
-                + " studentNumber ='" + vo.getStudentNumber() + "',"
-                + " school ='" + vo.getSchool() + "',"
-                + " schoolNumber ='" + vo.getSchoolNumber() + "',"
-                + " location ='" + vo.getLocation() + "',"
-                + " subsidy ='" + vo.getSubsidy() + "' where id=" + vo.getId();
-        return jdbcTemplate.update(sql);
+        int result = sqlSession.update("Board.updateBoard", vo);
+        return result;
     }
 
     public BoardVO getBoard(int id) {
-        String sql = "select * from SchoolBoard where id=" + id;
-        return jdbcTemplate.queryForObject(sql, new BoardRowMapper());
+        BoardVO result = sqlSession.selectOne("Board.getBoard", id);
+        return result;
     }
 
-    public List<BoardVO> getBoardList(){
-        String sql = "select * from SchoolBoard order by applyDate desc";
-        return jdbcTemplate.query(sql, new BoardRowMapper());
+    public List<BoardVO> getBoardList() {
+        List<BoardVO> result = sqlSession.selectList("Board.getBoardList");
+        return result;
     }
 }
